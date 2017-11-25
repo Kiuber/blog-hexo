fdcf54309132ec9a7ff728d08b0baad2af08251e
Kiuber <kiuber.zhang@gmail.com>
2017-11-25 14:59:11 +0800

    add server script implement auto generate files

---
+++ server_script.sh
@@ -0,0 +1,19 @@
+#!bash
+
+# add to timing tasks before use by the following command
+# crontab -e
+
+# pull latest code
+git pull
+
+# delete local hexo database
+hexo clean
+
+# generate blog folders and files
+hexo g
+
+# delete old blog folders and files
+rm -rf /usr/share/nginx/html/hi/blog/*
+
+# copy new blog folders and files to nginx html path
+cp -R public/* /usr/share/nginx/html/hi/blog/
